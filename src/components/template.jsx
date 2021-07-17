0a9ed2ff20e1d0348422996f639c3bbeb93ed73f
dcr7978933k <b219021@iiit-bh.ac.in>
2021-07-16 23:34:44 -0700

    first commit

---
+++ src/components/template.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+const Templates=({templates,setMeme})=>{
+  const goBack = () => (
+    <div>
+      <Link className="but2" to="/">
+        Go Back
+      </Link>
+    </div>
+  );
+ 
+    return (
+        <div className="temps">
+          <h2>Choose Template</h2>
+          {
+            goBack()
+          }
+             {
+        templates.map((temp)=>
+          (
+          <div key={temp.id} className="temp" onClick={()=>{
+            setMeme(temp)
+          }}>
+            <div className="image">
+            <div style={{backgroundImage:`url(${temp.url})`}} className='image'></div>
+            </div>
+          </div>
+
+        ))
+      };
+        </div>
+       
+    )
+}
+
+export default Templates;
---
+++ src/home.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+
+
+
+const Home = () => {
+  //  var imgs=document.querySelector(".images")
+  const [memes, setMemes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://meme-api.herokuapp.com/gimme/8")
+      .then((res) => res.json())
+      .then((data) => {
+        setMemes(data.memes);
+      
+      });
+  }, []);
+
+
+  const goBack = () => (
+    <div>
+      <Link className="but2" to="/create">
+        Create
+      </Link>
+    </div>
+  );
+  return (
+    
+      <div className="contain">
+        <h2 className="text">Generate + Create Memes</h2>
+
+        <button
+          className="but"
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          Generate
+        </button>
+        {
+        goBack()
+        }
+        <div className="canvas1">
+          <div className="images">
+            {memes.map((element, index) => (
+              <div key={index}  className="image1"  style={{ backgroundImage: `url(${element.url})` }}>
+              </div>
+            )
+)}
+          </div>
+        </div>
+      </div>
+   
+  );
+};
+
+export default Home;
